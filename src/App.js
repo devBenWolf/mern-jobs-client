360132b4e365c5664d8000ef6cabc6aa40c8fb80
BenJohannson <57221492+BenJohannson@users.noreply.github.com>
2022-08-27 09:13:52 -0400

    Update App.js
    Deleted create-react-app html boilerplate

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import Layout from "./Layout/Layout";
+import Landing from "./pages/Landing";
+import {BrowserRouter, Routes, Route} from "react-router-dom"
+import { Dashboard, Error, Register } from "./pages";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+    <Layout>
+
+      <Routes>
+          <Route path="/" element={<Dashboard />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="*" element={<Error />} />    
+          <Route path="/landing" element={<Landing />} />  
+      </Routes>
+    </Layout>
+    </BrowserRouter>
   );
 }
 
