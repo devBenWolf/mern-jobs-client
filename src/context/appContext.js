be4f8436a24a2f453a04e105e3e480699d2799ec
BenJohannson <57221492+BenJohannson@users.noreply.github.com>
2022-09-16 17:26:49 -0400

    Update appContext.js

--- src/context/appContext.js
+++ src/context/appContext.js
@@ -1,23 +1,40 @@
-import {useState, useReducer, useContext, createContext} from "react"
+import {useReducer, useContext, createContext} from "react"
+import { CLEAR_ALERT, DISPLAY_ALERT } from "./actions"
+import reducer from "./reducer"
 
-export const initialState = {
+const initialState = {
     isLoading: false,
-    showAlert: false,
+    showAlert: true,
     alertText: '',
     alertType: ''
 }
 
-export const AppContext = createContext()
+const AppContext = createContext()
 const AppContextProvider = ({children}) => {
-    const [state, setState] = useState(initialState)
+    const [state, dispatch] = useReducer(reducer, initialState)
+
+    const displayAlert = () => {
+        dispatch({type: DISPLAY_ALERT})
+        clearAlert()
+    }
+
+    const clearAlert = () => {
+        setTimeout(() => {
+            dispatch({type: CLEAR_ALERT})
+        }, 3000)
+    }
+
 
     return (
-        <AppContext.Provider value={{...state}}>
+        <AppContext.Provider value={{...state, displayAlert}}>
             {children}
         </AppContext.Provider>
     )
 }
 
+const useAppContext = () => {
+    return useContext(AppContext)
+}
 
-export {AppContextProvider}
+export {AppContextProvider, useAppContext, initialState}
 
